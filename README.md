1db95dc3cf674c57d8f3402e1debc14dd31b2667
elvin <elvinsavio.lp@gmail.com>
2021-05-30 02:07:38 +0530

    changes

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
  
-## Notes
+## mods
 -xstate
 -react-rnd
+-react-icons
+
+
+## ----
+
+header:
+    time
+    time - hover, click to display calendar
--- app/src/components/header/header_components/time.tsx
+++ app/src/components/header/header_components/time.tsx
@@ -1,26 +1,36 @@
 import { useEffect, useState } from 'react'
 import { BiAlarmOff, BiAlarm } from 'react-icons/bi'
 import { createMachine } from 'xstate'
+import { useMachine } from '@xstate/react'
 import Calendar from './calendar'
 import './header_components.css'
 
-const calendar_display_button_state = createMachine({
-  id: 'button_toggle',
+const Calendar_display_state =  createMachine({
+  id: 'calendar_display_state',
   initial: 'display_off',
   states: {
-    display_off : {},
-    display_on: {},
-    disaply_min:{}
+    display_off: {
+      on: {
+        display_calendar_button: "display_on",
+      }
+    },
+    display_on: {
+      on: {
+        display_calendar_button: "display_off",
+      }
+    }
   }
 })
 
 
 
+
 const Time = () => {
   const alarm =   null //alarm set
-  const [ time, setTime ] = useState(new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}));
+  const [ time, setTime ] = useState(new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}))
+  const [ current, send ] = useMachine(Calendar_display_state)
 
-  
+  console.log(current)
   useEffect(() => {
     const timer = setInterval(() => {      
       setTime(new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}))
@@ -30,8 +40,8 @@ const Time = () => {
 
   return(
     <>
-          <div className='display-time' >
-            { time } &nbsp; { alarm ? <BiAlarm /> :  <BiAlarmOff /> }
+          <div className='display-time' onClick={() => send("display_calenday_button")} >
+            { time } &nbsp; { alarm ? <BiAlarm /> :  <BiAlarmOff /> } 
           </div>
         <Calendar />
     </>
